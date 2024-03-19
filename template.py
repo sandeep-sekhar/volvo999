75796b2eb13700198665913426c13fc474c2a10c
sandeep-sekhar <sekharsandeep4@gmail.com>
2024-03-19 12:57:06 +0530

    project renew

--- template.py
+++ template.py
@@ -1,48 +1,48 @@
-import os
 from pathlib import Path
 import logging
+import os
+
 
 logging.basicConfig(level=logging.INFO)
 
-project_name='mlproject'
+project_name="mlproject"
 
 list_of_files=[
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/components/data_ingestion.py",
     f"src/{project_name}/components/data_transformation.py",
-    f"src/{project_name}/components/data_validation.py",
-    f"src/{project_name}/components/model_trainer.py",
+    f"src/{project_name}/components/model_tranier.py",
     f"src/{project_name}/components/model_monitering.py",
+    f"src/{project_name}/pipelines/__init__.py",
+    f"src/{project_name}/pipelines/training_pipeline.py",
+    f"src/{project_name}/pipelines/prediction_pipeline.py",
     f"src/{project_name}/exception.py",
     f"src/{project_name}/logger.py",
-    f"src/{project_name}/pipeline/__init__.py",
-    f"src/{project_name}/pipeline/training_pipeline.py",
-    f"src/{project_name}/pipeline/prediction_pipeline.py",
     f"src/{project_name}/utils.py",
+    "high.py"
+    "main.py",
     "app.py",
-    "Dockerfile"
+    "Dockerfile",
     "requirements.txt",
     "setup.py"
 ]
 
-
 for filepath in list_of_files:
-    filepath=Path(filepath)
-
-    filedir,filename=os.path.split(filepath)
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
 
     if filedir != "":
-        os.makedirs(filedir,exist_ok=True)
-        logging.info(f"creating directory:{filedir} for the file {filename}")
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory:{filedir} for the file {filename}")
 
-        if (not os.path.exists(filename)) or (os.path.getsize(filename)==0):
-            with open (filepath,"w") as f:
-                pass
-            logging.info(f"creating empty file:{filename}")
-                
+    
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath,'w') as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
 
-        else:
-            logging.info(f"{filename} is already created")
 
-        
+    
+    else:
+        logging.info(f"{filename} is already exists")
